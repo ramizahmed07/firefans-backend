02ce611810af20afd9ba0c1783f27ae271f3d626
Hassan <hassan.k.athumani@gmail.com>
2020-12-18 11:58:00 +0300

    mongoDB config file set up

--- back_end/mongoDB/mongo_config.py
+++ back_end/mongoDB/mongo_config.py
@@ -2,7 +2,7 @@ from configparser import ConfigParser
 import os
 
 
-def read_config(filename=os.getcwd()+'/mongo.ini', section='mongoDB'):
+def read_config(filename=os.getcwd()+'/mongoDB/mongo.ini', section='mongoDB'):
     """ Read database configuration file and return a dictionary object
     :param filename: name of the configuration file
     :param section: section of database configuration
